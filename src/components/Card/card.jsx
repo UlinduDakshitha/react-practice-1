c7a61c2cfa9a3ff02f666ea26b7f467533b13a28
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-12 09:25:51 +0530

    update

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -5,38 +5,11 @@
    return (
       <div className="id-card">
       {/* Header */}
-      <div className="id-card-header top-dark"></div><br />
+      <div className="id-card-header top-dark"></div><br/>
 
       <div className="id-card-header top-light"></div>
 
-      {/* Content */}
-      <div className="id-card-content">
-        {/* Profile image */}
-        <div className="profile-pic">👤</div>
-
-        {/* Details */}
-        <div className="details">
-          <h2 className="title">ID CARD</h2>
-
-          <p><strong>ID:</strong> 12345678912345678</p>
-          <p><strong>Name:</strong> NAME SURNAME</p>
-          <p>
-            <strong>Address:</strong><br />
-            123 ANYWHERE STREET<br />
-            CITY, 12345-875.
-          </p>
-          <p><strong>Phone:</strong> +5581487898</p>
-
-          {/* Footer */}
-          <div className="footer">
-            <div className="barcode"></div>
-            <div className="exp-date">
-              <strong>Expedition Date:</strong><br />
-              00/00/00
-            </div>
-          </div>
-        </div>
-      </div>
+     
     </div>
    )
  }
