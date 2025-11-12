3a3df617c07a9cb93fd1958d11d1a8033f59507c
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-12 09:15:31 +0530

    update

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,17 @@
 import { colors } from '@mui/material'
 import React from 'react'
 import './home.css'
-import card from '../../components/Card/card'
+ import Card from '../../components/Card/card'
+ 
+ 
 
 function Home() {
   return (
-    <div>
-        <h1 className='header'>Check Nic Details</h1>
-        <card/>
-    </div>
+    <>
+    <h1 className='header'>Check Nic Details</h1>
+    <Card/>
+
+    </>
   )
 }
 
