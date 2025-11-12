8c0a0734cdd1f78c4f742ea9230f86805d750d49
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-12 18:48:40 +0530

    update

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -1,15 +1,16 @@
  import React from 'react'
  import './card.css'
+ import Avatar from '@mui/material/Avatar';
  
  function Card() {
    return (
       <div className="id-card">
-      {/* Header */}
+      
       <div className="id-card-header top-dark"></div><br/>
 
       <div className="id-card-header top-light"></div>
 
-     
+     <Avatar src="/broken-image.jpg"  sx={{marginLeft:"20px",marginTop:"20px",borderRadius:"100px",width:"90px",height:"90px"}}/>
     </div>
    )
  }
