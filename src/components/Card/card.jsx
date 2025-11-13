e1cebc058b96a090cde4a0ae37abfd8e046c65b2
UlinduDakshitha <ulindudakshitha001@gmail.com>
2025-11-13 08:33:25 +0530

    update

--- src/components/Card/card.jsx
+++ src/components/Card/card.jsx
@@ -13,8 +13,21 @@
      <Avatar src="/broken-image.jpg"  sx={{marginLeft:"20px",marginTop:"20px",borderRadius:"100px",width:"90px",height:"90px"}}/>
 <div className='barcode'></div>
 
+         <h2 className='header2'>ID CARD</h2>
+         <p className='p1'>ID: 123456789123456789</p>
+         <br />
+         <p className='p2'>Name: <br />NAME SURNAME</p>
+<br />
+         <p className='p3'>Address: <br />123 ANYWERE STREET <br />CITY,12345-875.</p>
+         <br />
+         <p className='p4'>Phone: <br />+5581487898 </p>
+          <p className='p5'>Expedition Date:</p>
     </div>
    )
  }
 
  export default Card
+
+
+
+
